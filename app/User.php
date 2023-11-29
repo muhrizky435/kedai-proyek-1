a56e62a0872837cc922e1bcfb2114eecb8fd6dbb
Zmall02 <nidzhom.akmal22@student.polindra.ac.id>
2023-11-29 19:36:48 +0700

    menambahkan user

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ while ($record = mysqli_fetch_array($query)) {
 <div class="col-lg-9 mt-2">
   <div class="card">
     <div class="card-header">
-      Halaman User
+      Halaman User 
     </div>
     <div class="card-body">
       <div class="row">
@@ -17,10 +17,6 @@ while ($record = mysqli_fetch_array($query)) {
         </div>
         <div class="card-body">
             <div class="row">
-                <div class="col d-flex justify-content-end">
-                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalTambahUser"> Tambah
-                        User</button>
-                </div>
             </div>
             <!-- Modal -->
             <div class="modal fade" id="ModalTambahUser" tabindex="-1" aria-labelledby="exampleModalLabel"
@@ -40,7 +36,7 @@ while ($record = mysqli_fetch_array($query)) {
                                                 placeholder="Your Name" name="nama" required>
                                             <label for="floatingInput">Nama</label>
                                             <div class="invalid-feedback">
-                                                Masukan Nama.
+                                                Masukkan Nama.
                                             </div>
                                         </div>
                                     </div>
@@ -50,7 +46,7 @@ while ($record = mysqli_fetch_array($query)) {
                                                 placeholder="name@example.com" name="username" required>
                                             <label for="floatingInput">Username</label>
                                             <div class="invalid-feedback">
-                                                Masukan Username.
+                                                Masukkan Username.
                                             </div>
                                         </div>
                                     </div>
@@ -186,7 +182,7 @@ while ($record = mysqli_fetch_array($query)) {
                                         <?php echo $row['username'] ?>
                                     </td>
                                     <td>
-                                        <?php echo $row['nama'] ?>
+                                        <?php echo $row['Nama'] ?>
                                     </td>
                                     <td>
                                         <?php echo $row['level'] ?>
@@ -212,137 +208,23 @@ while ($record = mysqli_fetch_array($query)) {
             ?>
         </div>
       </div>
-      <!-- Modal-->
-      <div class="modal fade" id="ModalView" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-fullscreeen-md-down">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Data User</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- modal edit -->
-      <div class="modal fade" id="Modaledit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-fullscreeen-md-down">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Edit User</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- modal hapus -->
-      <div class="modal fade" id="Modalhapus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-fullscreeen-md-down">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">hapus User</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <?php
-      if (empty($result)) {
-        echo "Data User tidak ada";
-      } else {
-
-
-        ?>
-        <div class="table-responsive">
-          <table class="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">No</th>
-                <th scope="col">Username</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Level</th>
-                <th scope="col">No HP</th>
-                <th scope="col">Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              $no = 1;
-              foreach ($result as $row) {
-
-                ?>
-                <tr>
-                  <th scope="row">
-                    <?php echo $no++ ?>
-                  </th>
-                  <td>
-                    <?php echo $row['username'] ?>
-                  </td>
-                  <td>
-                    <?php echo $row['nama'] ?>
-                  </td>
-                  <td>
-                    <?php echo $row['level'] ?>
-                  </td>
-                  <td>
-                    <?php echo $row['No_Hp'] ?>
-                  </td>
-                  <td class="d-flex">
-                    <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal" data-bs-target="#ModalView"><i
-                        class="bi bi-eye"></i></button>
-                    <button class="btn btn-warning btn-sm me-1" data-bs-toggle="modal" data-bs-target="#Modaledit"><i
-                        class="bi bi-pencil-square"></i></button>
-                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#Modalhapus"><i
-                        class="bi bi-trash"></i></button>
-                  </td>
-                </tr>
-              <?php } ?>
-            </tbody>
-          </table>
-        </div>
-        <?php
-      }
-      ?>
-    </div>
-</div>
-
 <script>
     (() => {
         'use strict'
         const forms = document.querySelectorAll('.needs-validation')
         
         array.from(forms).forEach(form => {
-            form.addEventListener('submit', event => {
+          form.addEventListener('submit', event => {
                 if (!form.checkValidity()) {
                     event.preventDefault()
                     event.stopPropagation()
-                }
-
-                form.classList.add('was-validated')
+              }
+              form.classList.add('was-validated')
             }, false)
+        }
             )
         })
         
-    }) ()
-    </script>
+()
+</script>
     
--- proses/proses_input_user.php
+++ proses/proses_input_user.php
@@ -5,9 +5,11 @@ $username = (isset($_POST["username"])) ? htmlentities($_POST['username']) : "";
 $level = (isset($_POST["level"])) ? htmlentities($_POST['level']) : "";
 $nohp = (isset($_POST["nohp"])) ? htmlentities($_POST['nohp']) : "";
 $alamat = (isset($_POST["alamat"])) ? htmlentities($_POST['alamat']) : "";
+$password = (isset($_POST["password"])) ? md5(htmlentities($_POST["password"])) : "";
 
 if(!empty($_POST['input_user_validate'])){
-    $query = mysqli_query($koneksi, "INSERT INTO user (nama,username,level,nohp,alamat) value ('$name','$username','$level','$nohp','$alamat')");
+    $query = mysqli_query($koneksi, "INSERT INTO user (nama,username,level,nohp,alamat,password) 
+    values ('$name','$username','$level','$nohp','$alamat', '$password')");
     if(!$query){
         $massage = '<script>alert("Data gagal dimasukan")</script>';
     }else{
