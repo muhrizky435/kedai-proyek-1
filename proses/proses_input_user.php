43f781850b82de82a044614ad8f832d9ad1903a3
muhrizky435 <muhrizky435@gmail.com>
2023-11-30 09:17:00 +0700

    +

--- proses/proses_input_user.php
+++ proses/proses_input_user.php
@@ -1,21 +1,20 @@
 <?php
 include "connect.php";
-$name = (isset($_POST["nama"])) ? htmlentities($_POST['nama']) : "";
+$name = (isset($_POST["Nama"])) ? htmlentities($_POST['Nama']) : "";
 $username = (isset($_POST["username"])) ? htmlentities($_POST['username']) : "";
 $level = (isset($_POST["level"])) ? htmlentities($_POST['level']) : "";
-$nohp = (isset($_POST["nohp"])) ? htmlentities($_POST['nohp']) : "";
-$alamat = (isset($_POST["alamat"])) ? htmlentities($_POST['alamat']) : "";
-$password = (isset($_POST["password"])) ? md5(htmlentities($_POST["password"])) : "";
+$nohp = (isset($_POST["No_Hp"])) ? htmlentities($_POST['No_Hp']) : "";
+$password = md5('password');
 
 if(!empty($_POST['input_user_validate'])){
-    $query = mysqli_query($koneksi, "INSERT INTO user (nama,username,level,nohp,alamat,password) 
-    values ('$name','$username','$level','$nohp','$alamat', '$password')");
+    $query = mysqli_query($koneksi, "INSERT INTO pengguna (Nama,username,level,No_Hp,password) 
+    values ('$name','$username','$level','$nohp','$password')");
     if(!$query){
-        $massage = '<script>alert("Data gagal dimasukan")</script>';
+        $message = '<script>alert("Data gagal dimasukan")</script>';
     }else{
-        $massage = '<script>alert("Data berhasil dimasukan");
-                    window.location="../user"</script>
+        $message = '<script>alert("Data berhasil dimasukan");
+                    window.location="../app/User"</script>
                     </script>';
     }
-}echo $massage;
+}echo $message;
 ?>
